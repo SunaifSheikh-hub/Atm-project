e1f44b51d6236301889f867a8c5db1376c026f9c
Sunaif Sheikh <81877235+TalhaNaveed786-hub@users.noreply.github.com>
2022-01-17 20:40:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+// default funtion for whole ATM Task
+const atmFunc = () => {
+    // for getting the input and preference values 
+    reqAmountINPUT = document.getElementById("reqAmount").value;
+    reqAmountINPUT = Number(reqAmountINPUT);
+    prefer = document.getElementById("ReqPrefer").value;
+    prefer = Number(prefer);
+    noOfPrefer = null
+
+//  declaring the types of notes 
+    let notesarray = [500, 100, 50, 20, 10, 5, 1];
+
+    for (let i = 200; i > 0; i--) {
+        if (reqAmountINPUT / prefer >= i) {
+            reqAmountINPUT -= prefer * i;
+            noOfPrefer = i
+            break
+        }
+    }
+// for limiting the number of prefer notes 
+    for (var i = 0; i < notesarray.length; i++) {
+            if (notesarray[i] === prefer) {
+            let fordiv = document.getElementById("context")
+            let newin = document.createElement("p")
+            newin.innerHTML = `${notesarray[i]}  No. Of Notes : ${noOfPrefer} <br>  Balance Amount :   ${reqAmountINPUT}`
+            fordiv.appendChild(newin);
+        }
+        else{
+            qty = Math.floor(reqAmountINPUT / notesarray[i]);
+            reqAmountINPUT -= qty * notesarray[i];
+            let fordiv = document.getElementById("context")
+            let newin = document.createElement("p")
+            newin.innerHTML = `${notesarray[i]}  No. Of Notes : ${qty} <br>  Balance Amount :   ${reqAmountINPUT}`
+            fordiv.appendChild(newin);
+        }
+    }
+}
+
+
+
+// // default function for whole ATM Task 
+// const atmFunc = () => {
+//     reqAmountINPUT = document.getElementById("reqAmountINPUT").value;
+//     reqAmountINPUT = Number(reqAmountINPUT)
+//     prefer = document.getElementById("ReqPrefer").value;
+//     prefer = Number(prefer);
+//     noofPrefer = null;
+// // declaring the types of notes
+//     let notesarray = [500, 100, 50, 20, 10, 5, 1];
+// // for limiting the number of prefer notes
+//     for (let i = 200; i > 0; i--) {
+//     if (reqAmountINPUT / prefer >= i) {
+//     reqAmountINPUT -= prefer * i
+//     noofPrefer = i    
+//     break
+//     }       
+//     }
+
+//     for (var i = 0; i < notesarray.length; i++) {
+//         if (notesarray[i] == prefer) {
+//         let fordiv = document.getElementById("context")
+//         let newfor = document.createElement("p");
+//         newfor.innerHTML = `${notesarray[i]} No of Notes : ${noofPrefer} <br> Balance Amount ${reqAmountINPUT}`
+//         fordiv.appendChild(newfor);
+//     } else {
+//         qty = Math.floor(reqAmountINPUT / notesarray[i])
+//         reqAmountINPUT -= qty * notesarray[i];
+//         let fordiv = document.getElementById("context");
+//         let newfor = document.createElement("p");
+//         newfor.innerHTML = `${notesarray[i]} No of Notes : ${noofPrefer} <br> Balance Amount ${reqAmountINPUT}`
+//         fordiv.appendChild(newfor);
+//     } 
+//     }
+
+// }
